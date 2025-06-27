b582f91afc672f2c290fe6128de4bb4b99363adc
Andrés Savaria <a.savaria@duocuc.cl>
2025-06-26 22:31:53 -0400

    Update menu_base.py
    Agrega función de Andrés (Integrante 1)

--- menu_base.py
+++ menu_base.py
@@ -1,9 +1,12 @@
+# Función personalizada del integrante 1
+def datos_andres():
+    print("Mi nombre es Andrés Savaria y tengo 26 años.")
+
 # Menú base del programa 
 while True: 
     print("\n--- MENÚ PRINCIPAL ---") 
     print("1. Función de integrante 1") 
-    print("2. Función de integrante 2") 
-    print("3. Función de integrante 3") 
+    print("2. Función de integrante 2")  
     print("0. Salir") 
 
     op = input("Seleccione opción: ") 
@@ -12,7 +15,7 @@ while True:
         print("Programa finalizado.") 
         break 
     elif op == "1": 
-        pass  # Aquí se llamará a la función del integrante 1 
+        datos_andres() # LLama a la función del integrante 1 
     elif op == "2": 
         pass  # Aquí se llamará a la función del integrante 2 
     else: 
